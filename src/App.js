b8a87f94fd0e0081a16dd78619ee023299520d6e
eric <ecomstock@gmail.com>
2019-12-07 10:33:08 -0800

    new: add static placeholders and enclose in card

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
+import Card from '@material-ui/core/Card';
 
 class App extends Component {
 
   	render() {
 		return (
-			<div className="container">
-				<div className="header">
-					<img src="https://via.placeholder.com/75" />
-					<p>Sunbuddy</p>
-				</div>
+			<Card className="card">
 				<div className="content">
-					<img src="https://via.placeholder.com/25" />
-					<p></p>
+					<img src="https://via.placeholder.com/75" />
+					<p className="text-large">Portland</p>
+					<p>today</p>
+					<div className="sub-container">
+						<p>32</p>
+						<p className="text-large">40</p>
+						<p>42</p>
+					</div>
+					<div className="sub-container">
+						<img src="https://via.placeholder.com/25" />
+						<p>7:10 am - 5:03 pm</p>
+					</div>
+					<div className="sub-container">
+						<img src="https://via.placeholder.com/25" />
+						<p>7:10 am - 5:03 pm</p>
+					</div>
+					<div className="sub-container">
+						<img src="https://via.placeholder.com/25" />
+						<p>7:10 am - 5:03 pm</p>
+					</div>
+					<div className="sub-container">
+						<img src="https://via.placeholder.com/25" />
+						<p>7:10 am - 5:03 pm</p>
+					</div>
 				</div>
-			</div>
+			</Card>
 		);
   	}
 }
