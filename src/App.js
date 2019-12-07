b347cbb171d9c73d5a6603d24f0b46151bb7bdad
eric <ecomstock@gmail.com>
2019-12-07 10:34:15 -0800

    new: add fetch method

--- src/App.js
+++ src/App.js
@@ -4,6 +4,29 @@ import Card from '@material-ui/core/Card';
 
 class App extends Component {
 
+
+	componentDidMount() {
+		fetch("https://api.example.com/items")
+			.then(res => res.json())
+			.then(
+				(result) => {
+				this.setState({
+					isLoaded: true,
+					items: result.items
+				});
+				},
+				// Note: it's important to handle errors here
+				// instead of a catch() block so that we don't swallow
+				// exceptions from actual bugs in components.
+				(error) => {
+				this.setState({
+					isLoaded: true,
+					error
+				});
+				}
+			)
+	}
+
   	render() {
 		return (
 			<Card className="card">
