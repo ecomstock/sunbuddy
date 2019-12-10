292f7be7e567c46415d81aef30bc977a3f9301f8
eric <ecomstock@gmail.com>
2019-12-09 18:50:19 -0800

    wip: add core methods

--- src/App.js
+++ src/App.js
@@ -4,8 +4,32 @@ import Card from '@material-ui/core/Card';
 
 class App extends Component {
 
+	componentDidMount () {
+		console.log("loaded");
+		this.getNav();
+	}
 
-	componentDidMount() {
+	// success = (position) => {
+	getLatLon (position) {
+		console.log("nav success");
+		const latitude  = position.coords.latitude;
+		const longitude = position.coords.longitude;
+		const latlon = [latitude, longitude];
+		this.sayHi();
+		console.log(latitude);
+	}
+
+	sayHi () {
+		console.log("hi");
+	}
+
+	getNav () {
+		navigator.geolocation.getCurrentPosition(this.getLatLon.bind(this));
+	}
+
+	runFetch (latlon) {
+		const latitude = latlon[0];
+		const longitude = latlon[1];
 		fetch("https://api.example.com/items")
 			.then(res => res.json())
 			.then(
@@ -27,38 +51,39 @@ class App extends Component {
 			)
 	}
 
-  	render() {
-		return (
-			<Card className="card">
-				<div className="content">
-					<img src="https://via.placeholder.com/75" />
-					<p className="text-large">Portland</p>
-					<p>today</p>
-					<div className="sub-container">
-						<p>32</p>
-						<p className="text-large">40</p>
-						<p>42</p>
-					</div>
-					<div className="sub-container">
-						<img src="https://via.placeholder.com/25" />
-						<p>7:10 am - 5:03 pm</p>
-					</div>
-					<div className="sub-container">
-						<img src="https://via.placeholder.com/25" />
-						<p>7:10 am - 5:03 pm</p>
-					</div>
-					<div className="sub-container">
-						<img src="https://via.placeholder.com/25" />
-						<p>7:10 am - 5:03 pm</p>
-					</div>
-					<div className="sub-container">
-						<img src="https://via.placeholder.com/25" />
-						<p>7:10 am - 5:03 pm</p>
-					</div>
+	element = (
+		<Card className="card">
+			<div className="content">
+				<img src="https://via.placeholder.com/75" />
+				<p className="text-large">Portland</p>
+				<p>today</p>
+				<div className="sub-container">
+					<p>32</p>
+					<p className="text-large">40</p>
+					<p>42</p>
 				</div>
-			</Card>
-		);
-  	}
+				<div className="sub-container">
+					<img src="https://via.placeholder.com/25" />
+					<p>7:10 am - 5:03 pm</p>
+				</div>
+				<div className="sub-container">
+					<img src="https://via.placeholder.com/25" />
+					<p>7:10 am - 5:03 pm</p>
+				</div>
+				<div className="sub-container">
+					<img src="https://via.placeholder.com/25" />
+					<p>7:10 am - 5:03 pm</p>
+				</div>
+				<div className="sub-container">
+					<img src="https://via.placeholder.com/25" />
+					<p>7:10 am - 5:03 pm</p>
+				</div>
+			</div>
+		</Card>
+	);
+
+  	render = () => this.element;
+
 }
 
 export default App;
