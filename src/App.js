f7a434b9f03ac78dc8da369cb5ae712eee024fff
eric <ecomstock@gmail.com>
2019-12-13 13:39:00 -0800

    update: add darksky api and use cors anywhere

--- src/App.js
+++ src/App.js
@@ -21,17 +21,18 @@ class App extends Component {
 	
 	getUVData = latlon => {
 		console.log(latlon);
-		const latitude = latlon[0];
-		const longitude = latlon[1];
-		const url = `https://api.openuv.io/api/v1/uv?lat=${latitude}&lng=${longitude}`
-		fetch(url, {
-			headers: {
-				"x-access-token" : "8a2198fe4c4d3912dbf995040b2ff964"
-			}
-		})
+		const lat = latlon[0];
+		const lon = latlon[1];
+		// const url = `https://api.openuv.io/api/v1/uv?lat=${lat}&lng=${lon}`;
+		const url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/c9390ab45282a0eb042232d180560a3d/${lat},${lon}`;
+		fetch(url)
+		// const id = "75eeb11ea6d63fece113632a7e10d285";
+		// const url = `http://api.openweathermap.org/data/2.5/uvi/forecast?appid=${id}&lat=${lat}&lon=${lon}&cnt=1`
+		// fetch(url)
 			.then(res => res.json())
 			.then(
 				(result) => {
+					console.log(result);
 					this.parseUVData(result);
 				// this.setState({
 				// 	isLoaded: true,
@@ -51,8 +52,18 @@ class App extends Component {
 	}
 
 	parseUVData = data => {
-		console.log(data);
+		console.log(data.result);
+		// const criticalUV = data.result
+		// 	.filter(dataPoint => dataPoint.uv > .5)
+		// 	.map(dataPoint => {
+		// 		const returnObj = {};
+		// 		returnObj.time = new Date(dataPoint.uv_time).toLocaleTimeString();
+		// 		return returnObj;
+		// 	});
+		//return `${criticalUV[0].time} - ${criticalUV[criticalUV.length-1].time}`;
 	}
+
+	exposureTime = "Blah";
 
 	element = (
 		<Card className="card">
@@ -67,7 +78,7 @@ class App extends Component {
 				</div>
 				<div className="sub-container">
 					<img src="https://via.placeholder.com/25" />
-					<p>7:10 am - 5:03 pm</p>
+					<p>{this.exposureTime}</p>
 				</div>
 				<div className="sub-container">
 					<img src="https://via.placeholder.com/25" />
