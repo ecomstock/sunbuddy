7e12303c3ccf664cbee4be904733dbe427a9a837
eric <ecomstock@gmail.com>
2019-12-15 17:57:09 -0800

    wip: refactor weather functions & add sun fetch

--- src/App.js
+++ src/App.js
@@ -5,35 +5,30 @@ import Card from '@material-ui/core/Card';
 class App extends Component {
 
 	componentDidMount () {
-		console.log("loaded");
 		this.getNav();
 	}
 
+	getNav = () => navigator.geolocation.getCurrentPosition(this.getLatLon);
+
 	getLatLon = position => {
-		console.log("nav success");
 		const latitude  = position.coords.latitude;
 		const longitude = position.coords.longitude;
-		const latlon = [latitude, longitude];
-		this.getUVData(latlon);
+		// const latlon = [latitude, longitude];
+		const latlon = [23, -110];
+		this.getWeatherData(latlon);
+		// this.getSunData(latlon);
 	}
-
-	getNav = () => navigator.geolocation.getCurrentPosition(this.getLatLon);
 	
-	getUVData = latlon => {
+	getWeatherData = latlon => {
 		console.log(latlon);
 		const lat = latlon[0];
 		const lon = latlon[1];
-		// const url = `https://api.openuv.io/api/v1/uv?lat=${lat}&lng=${lon}`;
 		const url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/c9390ab45282a0eb042232d180560a3d/${lat},${lon}`;
 		fetch(url)
-		// const id = "75eeb11ea6d63fece113632a7e10d285";
-		// const url = `http://api.openweathermap.org/data/2.5/uvi/forecast?appid=${id}&lat=${lat}&lon=${lon}&cnt=1`
-		// fetch(url)
 			.then(res => res.json())
 			.then(
 				(result) => {
-					console.log(result);
-					this.parseUVData(result);
+					this.distributeWeatherData(result);
 				// this.setState({
 				// 	isLoaded: true,
 				// 	items: result.items
@@ -51,17 +46,65 @@ class App extends Component {
 			)
 	}
 
-	parseUVData = data => {
-		console.log(data.result);
-		// const criticalUV = data.result
-		// 	.filter(dataPoint => dataPoint.uv > .5)
-		// 	.map(dataPoint => {
-		// 		const returnObj = {};
-		// 		returnObj.time = new Date(dataPoint.uv_time).toLocaleTimeString();
-		// 		return returnObj;
-		// 	});
+	// getSunData = latlon => {
+	// 	console.log(latlon);
+	// 	const lat = latlon[0];
+	// 	const lon = latlon[1];
+	// 	const url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/c9390ab45282a0eb042232d180560a3d/${lat},${lon}`;
+	// 	fetch(url)
+	// 	// const id = "75eeb11ea6d63fece113632a7e10d285";
+	// 	// const url = `http://api.openweathermap.org/data/2.5/uvi/forecast?appid=${id}&lat=${lat}&lon=${lon}&cnt=1`
+	// 	// fetch(url)
+	// 		.then(res => res.json())
+	// 		.then(
+	// 			(result) => {
+	// 				this.distributeSunData(result);
+	// 			// this.setState({
+	// 			// 	isLoaded: true,
+	// 			// 	items: result.items
+	// 			// });
+	// 			},
+	// 			// Note: it's important to handle errors here
+	// 			// instead of a catch() block so that we don't swallow
+	// 			// exceptions from actual bugs in components.
+	// 			(error) => {
+	// 			this.setState({
+	// 				isLoaded: true,
+	// 				error
+	// 			});
+	// 			}
+	// 		)
+	// }
+
+	distributeWeatherData = data => {
+		this.getExposureHours(data);
+		
+		const hourly = data.hourly.data;
+		
+	
+	}
+
+	// getDay = data {
+	// 	const today = new Date().getDay();
+	// }
+
+	getExposureHours = data => {
+		const hourly = data.hourly.data;
+		const exposureHours = hourly
+			.filter(dataPoint => dataPoint.uvIndex >= 2);
+			// .filter(dataPoint => new Date(dataPoint.time * 1000).getDay() === today)
+			// .map(dataPoint => {
+			// 	const returnObj = {};
+			// 	returnObj.time = new Date(dataPoint.time * 1000).toLocaleTimeString();
+			// 	return returnObj;
+			// });
+		console.log(exposureHours);
 		//return `${criticalUV[0].time} - ${criticalUV[criticalUV.length-1].time}`;
 	}
+
+	// getPrecipHours = data => {
+
+	// }
 
 	exposureTime = "Blah";
 
