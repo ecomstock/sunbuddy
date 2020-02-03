cca88fa8c0acbecdc64e2ffaa4ff67e5af444648
eric <ecomstock@gmail.com>
2020-02-03 10:22:58 -0800

    new: add api calls and render city

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
@@ -16,7 +16,6 @@ import WarningIcon from '@material-ui/icons/Warning';
 import DirectionsRunIcon from '@material-ui/icons/DirectionsRun';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import logo from "./logo.png";
 
 function Copyright() {
     return (
@@ -59,8 +58,77 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
-export default function SignInSide() {
+export default function App() {
     const classes = useStyles();
+
+    const conditions = {
+		uv     : "hour.uvIndex >= 4", 
+		temp   : "hour.apparentTemperature < 70 && hour.apparentTemperature > 32",
+		precip : "hour.precipIntensity >= 0.015 && hour.precipProbability >= 0.20"
+    }
+    
+    const [city, setCity] = React.useState();
+
+    useEffect(() => {
+        getNav();
+    });
+
+    const getNav = () => navigator.geolocation.getCurrentPosition(getLatLon);
+
+    const getLatLon = position => {
+		const coords = {};
+		coords.latitude  = position.coords.latitude;
+		coords.longitude = position.coords.longitude;
+		// coords.latitude  = 1;
+        // coords.longitude = 104;
+        getCity(coords);
+		getWeatherData(coords);
+    }
+    
+    const getCity = coords => {
+		const lat = coords.latitude;
+		const lon = coords.longitude; 
+		const url = `https://geocodeapi.p.rapidapi.com/GetLargestCities?latitude=${lat}&longitude=${lon}&range=50000`
+		fetch(url, {
+			"method": "GET",
+			"headers": {
+				"x-rapidapi-host": "geocodeapi.p.rapidapi.com",
+				"x-rapidapi-key": "G0GBgJ6WYSmshKfgZydJ6iBoKrThp11fTNTjsnljvjz33JtipZ"
+			}
+		})
+		.then(response => response.json())
+		.then(result => {
+			console.log(result);
+            const city = result[0].City;
+            setCity(city);
+		})
+		// .catch(err => {
+		// 	console.log(err);
+		// });
+	}
+	
+	const getWeatherData = coords => {
+		const lat = coords.latitude;
+		const lon = coords.longitude;
+		const url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/c9390ab45282a0eb042232d180560a3d/${lat},${lon}`;
+		fetch(url)
+			.then(res => res.json())
+			.then(
+				(result) => {
+					console.log(result);
+					//this.exportData(coords, result);
+				},
+				// Note: it's important to handle errors here
+				// instead of a catch() block so that we don't swallow
+				// exceptions from actual bugs in components.
+				// (error) => {
+				// this.setState({
+				// 	isLoaded: true,
+				// 	error
+				// });
+				// }
+			)
+	}
 
     return (
         <Grid container component="main" className={classes.root}>
@@ -69,7 +137,7 @@ export default function SignInSide() {
             <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
                 <div className={classes.paper}>
                     <Typography>SUNBUDDY</Typography>
-                    <Typography>Portland</Typography>
+                    <Typography>{city}</Typography>
                     <Box item>
                         <Grid container spacing={1}>
                             <Grid item>
