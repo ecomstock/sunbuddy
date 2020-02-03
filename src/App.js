b15488808cbd3836d7a410c4ea2a5c5cbc9840e8
eric <ecomstock@gmail.com>
2020-02-03 09:24:26 -0800

    new: rm form, add time display list

--- src/App.js
+++ src/App.js
@@ -1,130 +1,119 @@
 import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import WbSunnyIcon from '@material-ui/icons/WbSunny';
+import WbCloudyIcon from '@material-ui/icons/WbCloudy';
+import WarningIcon from '@material-ui/icons/Warning';
+import DirectionsRunIcon from '@material-ui/icons/DirectionsRun';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
 function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Sunbuddy
-      </Link>{' '}
-      {new Date().getFullYear()}
-    </Typography>
-  );
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+        {'Copyright © '}
+        <Link color="inherit" href="https://material-ui.com/">
+            Sunbuddy
+        </Link>{' '}
+        {new Date().getFullYear()}
+        </Typography>
+    );
 }
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    height: '100vh',
-  },
-  image: {
-    backgroundImage: 'url(https://source.unsplash.com/random)',
-    backgroundRepeat: 'no-repeat',
-    backgroundColor:
-      theme.palette.type === 'dark' ? theme.palette.grey[900] : theme.palette.grey[50],
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-  },
-  paper: {
-    margin: theme.spacing(8, 4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
+    root: {
+        height: '100vh',
+    },
+    image: {
+        //backgroundImage: 'url(https://source.unsplash.com/J0wKylnjtNo)',
+        backgroundImage: 'url(https://source.unsplash.com/AtfA8NDgpKA)',
+        backgroundRepeat: 'no-repeat',
+        backgroundColor:
+            theme.palette.type === 'dark' ? theme.palette.grey[900] : theme.palette.grey[50],
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+    },
+    paper: {
+        margin: theme.spacing(8, 4),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
 }));
 
 export default function SignInSide() {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <Grid container component="main" className={classes.root}>
-      <CssBaseline />
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <form className={classes.form} noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
+    return (
+        <Grid container component="main" className={classes.root}>
+            <CssBaseline />
+            <Grid item xs={false} sm={4} md={7} className={classes.image} />
+            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                <div className={classes.paper}>
+                    {/* <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                    </Avatar> */}
+                    <Grid>
+                        {/* logo */}
+                        {/* place */}
+                        {/* temperatures */}
+                    </Grid>
+                    <List>
+                        <ListItem>
+                            <ListItemAvatar>
+                            <Avatar>
+                                <WbSunnyIcon />
+                            </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="10:00-6:00" secondary="Daylight hours" />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemAvatar>
+                            <Avatar>
+                                <WbCloudyIcon />
+                            </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="10:00-6:00" secondary="Precipitation is expected" />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemAvatar>
+                            <Avatar>
+                                <WarningIcon />
+                            </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="10:00-6:00" secondary="High UV &mdash; sunscreen is advisable" />
+                        </ListItem>
+                        <ListItem>
+                            <ListItemAvatar>
+                            <Avatar>
+                                <DirectionsRunIcon />
+                            </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary="10:00-6:00" secondary="Ideal temperature for outdoor exercise" />
+                        </ListItem>
+                    </List>
+                    <Box mt={8}>
+                        <Copyright />
+                    </Box>
+                </div>
             </Grid>
-            <Box mt={5}>
-              <Copyright />
-            </Box>
-          </form>
-        </div>
-      </Grid>
-    </Grid>
-  );
+        </Grid>
+    );
 }
