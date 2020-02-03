4ff5d28edcefb32fefc89d5d65b5bdb5a413a6e9
eric <ecomstock@gmail.com>
2020-02-02 18:31:01 -0800

    update: swap div for box

--- src/Condition.js
+++ src/Condition.js
@@ -1,18 +1,20 @@
 import React from "react";
 import Icon from "@mdi/react";
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
 
 const Condition = props => {
     if (props.conditionTime) {
         return (
-            <div className="condition" >
+            <Box className="condition" >
                 <Icon className="icon"
                     path={props.iconName}
                     title={props.iconTitle}
                     size={props.iconSize}
                     color={props.iconColor}
                 />
-                <p>{props.conditionTime}</p>
-            </div>
+                <Typography variant="body1">{props.conditionTime}</Typography>
+            </Box>
         )
     }
     return null;
