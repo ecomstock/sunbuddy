aed5af119f5007d07b56d0547f0e16fa16a9f2cd
eric <ecomstock@gmail.com>
2020-03-12 20:15:01 -0700

    Updates

---
+++ precache-manifest.0f28e1a7d0b6cce1ecf0fe9b29984adb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35c053d043c3df22f43186019b0f25de",
+    "url": "/sunbuddy/index.html"
+  },
+  {
+    "revision": "9381afd9f9e9c2d99194",
+    "url": "/sunbuddy/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "bacc40468a38767f3cac",
+    "url": "/sunbuddy/static/js/2.343f6bf9.chunk.js"
+  },
+  {
+    "revision": "1065688f839bc1c9d3fb33e665bee848",
+    "url": "/sunbuddy/static/js/2.343f6bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9381afd9f9e9c2d99194",
+    "url": "/sunbuddy/static/js/main.a50b08e4.chunk.js"
+  },
+  {
+    "revision": "050547b9d5bab5a637c2",
+    "url": "/sunbuddy/static/js/runtime-main.694ca87a.js"
+  }
+]);
